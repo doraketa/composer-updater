ec25a1bb6028030cd89465532b0b8a9261637ac2
Vadim Polyakov <cachegetfield@yandex.ru>
2021-12-02 12:06:10 +0300

    Added a command to get a file to install Composer and determine the current CheckSum

--- updater.sh
+++ updater.sh
@@ -2,3 +2,9 @@
 
 # Installer Signature
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+
+# The command that receives the composer installer file
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+# Getting the current CheckSum
+CURRENT_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
