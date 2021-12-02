1bef42bce552c7ac10bc1bb253e11071b9de3f54
Vadim Polyakov <cachegetfield@yandex.ru>
2021-12-02 12:14:45 +0300

    The basic logic of checking amounts, message output, file deletion and logical interruption is implemented.

--- updater.sh
+++ updater.sh
@@ -8,3 +8,11 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 
 # Getting the current CheckSum
 CURRENT_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+# Condition if the amount does not match
+if [ "$EXPECTED_CHECKSUM" != "$CURRENT_CHECKSUM" ]
+then
+  >&2 echo 'ERROR: Incorrect verification amount'
+  rm composer-setup.php   # Deleting composer-installer file
+  exit 1                  # if incorrect arguments are passed to the script
+fi
